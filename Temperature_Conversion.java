fa10ee064b4c629920225662641c17fe0d875fb2
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-08-26 00:24:57 +0530

    import java.util.*;
    class pk{
        public static void main(String args[]){
            Scanner sc = new Scanner(System.in);
            float c = sc.nextFloat();
            float p = (c*9/5)+32;
            System.out.format("%.2f",p);
        }
    }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class pk{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float c = sc.nextFloat();
+        float p = (c*9/5)+32;
+        System.out.format("%.2f",p);
+    }
+}
