b7da58847ee3b0ef8c8ebe4e9191b47c86bbccc1
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-08-26 00:35:15 +0530

    import java.util.*;
    class pk{
        public static void main(String args[]){
            Scanner sc = new Scanner(System.in);
            float a = sc.nextFloat(); float b= sc.nextFloat();
            float avg = (a+b)/2;
            System.out.format("%.4f",avg);
        }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class pk{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat(); float b= sc.nextFloat();
+        float avg = (a+b)/2;
+        System.out.format("%.4f",avg);
+    }
+}
