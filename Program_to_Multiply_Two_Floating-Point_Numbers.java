5b13af5fa192477cba28eee517c21faab6b49018
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-08-26 00:42:04 +0530

    import java.util.*;
    class pk{
        public static void main(String args[]){
            Scanner sc = new Scanner(System.in);
            float a= sc.nextFloat();
            float b= sc.nextFloat();
            float mul = a*b;
            System.out.format("%.2f",mul);
        }
        
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class pk{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float a= sc.nextFloat();
+        float b= sc.nextFloat();
+        float mul = a*b;
+        System.out.format("%.2f",mul);
+    }
+    
+}
