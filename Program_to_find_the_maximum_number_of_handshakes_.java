3460bae7838b6d680ad1faef61bae03a5371f28b
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-08-26 01:24:21 +0530

    import java.util.*;
    class max{
        public static void main(String args[]){
            Scanner sc = new Scanner(System.in);
            int n = sc.nextInt();
            int p = (n*(n-1)/2);
            System.out.print(p);
        }
    }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class max{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int p = (n*(n-1)/2);
+        System.out.print(p);
+    }
+}
