58f799fa9d812fea83e413793949306be07644a4
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-08-26 03:21:09 +0530

    import java.util.*;
    class pandemic{
        public static void main(String args[]){
            Scanner sc = new Scanner(System.in);
            int n= sc.nextInt();
            if (n==2){
                System.out.print("1");
            }
            else if(n==1){
                 System.out.print("0");
            }
            else{
                 System.out.print("2");
            }
        }
    }

---
+++ The_Pandemic.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class pandemic{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        if (n==2){
+            System.out.print("1");
+        }
+        else if(n==1){
+             System.out.print("0");
+        }
+        else{
+             System.out.print("2");
+        }
+    }
+}
