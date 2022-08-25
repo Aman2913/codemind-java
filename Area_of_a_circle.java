faa91115b8c48487584a64889008b16bd98906d0
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-08-26 00:10:06 +0530

    import java.util.*;  class pk{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         double n = sc.nextInt();         double p= 3.14*n*n;         System.out.format("%.2f",p);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+ class pk{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextInt();
+        double p= 3.14*n*n;
+        System.out.format("%.2f",p);
+    }
+}
