738be8f70b5a4c2f6179d2a5e150aea3e58bcc6b
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-09-30 08:45:52 +0530

    import java.util.*;
    class conversion{
        public static void main(String args[]){
            Scanner sc = new Scanner(System.in);
            int a =sc.nextInt();
            for(int j=0;j<a;j++){
                long  n,i=0,d=0,b=0;
                n=sc.nextLong();
                while(n!=0)
                {
                    d+=(n%10)*Math.pow(8,i);
                    n/=10;
                    i++;
                }
                i=1;
                while(d!=0){
                    b+=(d%2)*i;
                    d/=2;
                    i*=10;
        }
        System.out.println(b);
        }
    }
    }

---
+++ Octal_to_Binary.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class conversion{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        for(int j=0;j<a;j++){
+            long  n,i=0,d=0,b=0;
+            n=sc.nextLong();
+            while(n!=0)
+            {
+                d+=(n%10)*Math.pow(8,i);
+                n/=10;
+                i++;
+            }
+            i=1;
+            while(d!=0){
+                b+=(d%2)*i;
+                d/=2;
+                i*=10;
+    }
+    System.out.println(b);
+    }
+}
+}
